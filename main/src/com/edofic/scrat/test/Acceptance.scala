47d0c00c068f32291178b6b505cd78466409c90b
Andraz Bajt <edofic@gmail.com>
2012-08-27 12:03:58 +0200

    added some basic tests

---
+++ main/src/com/edofic/scrat/test/Acceptance.scala
@@ -0,0 +1,35 @@
+package com.edofic.scrat.test
+
+import org.scalatest.FunSuite
+import com.edofic.scrat.ScratRuntime
+
+/**
+ * User: andraz
+ * Date: 8/27/12
+ * Time: 11:43 AM
+ */
+class Acceptance extends FunSuite {
+  val runtime = new ScratRuntime
+
+  test("addition and subtraction") {
+    val exp = "1+2+3+4-7-2"
+    val result = runtime.eval(exp)
+    val expected = 1 + 2 + 3 + 4 - 7 - 2
+    assert(result === expected)
+  }
+
+  test("multiplication and division") {
+    val exp = "1*3/5*7/2"
+    val result = runtime.eval(exp)
+    def d(n: Int) = n: Double
+    val expected = d(1) * d(3) / d(5) * d(7) / d(2)
+    assert(result === expected)
+  }
+
+  test("+-*/()^") {
+    val exp = "(1+3)*5/2^(5-2)"
+    val result = runtime.eval(exp)
+    val expected = (1 + 3) * 5 / math.pow(2, (5 - 2))
+    assert(result === expected)
+  }
+}
